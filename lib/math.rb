fa73641609be798a36b5d1518e3ba4439e2695a9
Marci Clark <marcig612@gmail.com>
2019-01-09 03:38:46 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,13 +1,25 @@
 def addition(num1, num2)
+  describe "#addition" do 
+  it "adds two numbers together" do
+    expect(addition(5,4)).to eq(9)
 end
 
 def subtraction(num1, num2)
+  desribe "#subtraction" do
+    it "subtracts two numbers" do
+      expect(subtraction(10,5)).to eq(5)
 end
 
 def division(num1, num2)
-end
+  describe "#division" do
+    it "divides two numbers" do
+      expect(division(50,2)).to eq (25)
+      end
 
 def multiplication(num1, num2)
+  describe "#multiplication" do
+    it "multiplies two numbers" do
+      expect(multiplication(10,10)).to eq (100)
 end
 
 def modulo(num1, num2)
